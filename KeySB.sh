93622453b39a09fa13aeb4c25a0143b962b3e7e9
fran-cloud <86373103+fran-cloud@users.noreply.github.com>
2024-04-01 13:45:34 +0200

    Add files via upload

---
+++ KeySB.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Create keys
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Genio PK/" -keyout PK.key \
+        -out PK.crt -days 3650 -nodes -sha256
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Genio KEK/" -keyout KEK.key \
+        -out KEK.crt -days 3650 -nodes -sha256
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Genio DB/" -keyout DB.key \
+        -out DB.crt -days 3650 -nodes -sha256
+
+#Create DER version
+openssl x509 -in PK.crt -out PK.cer -outform DER
+openssl x509 -in KEK.crt -out KEK.cer -outform DER
+openssl x509 -in DB.crt -out DB.cer -outform DER
+
+apt install uuid-runtime
+uuidgen --random > GUID.txt
+
+#Convert a certificate into a EFI signature list
+cert-to-efi-sig-list -g "$(< GUID.txt)" PK.crt PK.esl
+cert-to-efi-sig-list -g "$(< GUID.txt)" KEK.crt KEK.esl
+cert-to-efi-sig-list -g "$(< GUID.txt)" DB.crt DB.esl
+
+#Sign EFI signature list
+sign-efi-sig-list -g "$(< GUID.txt)" -k PK.key -c PK.crt PK PK.esl PK.auth
+sign-efi-sig-list -g "$(< GUID.txt)" -a -k PK.key -c PK.crt KEK KEK.esl KEK.auth
+sign-efi-sig-list -g "$(< GUID.txt)" -a -k KEK.key -c KEK.crt DB DB.esl DB.auth
+
+#Make the private key readable only by root
+chmod 0600 *.key
